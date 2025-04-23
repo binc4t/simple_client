1ae8609737b4ef55b756ef0a185b6c60b7dbcde3
binc4t <y980715@gmail.com>
2025-04-23 22:23:06 +0800

    f

--- main.go
+++ main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
+	"time"
 )
 
 func main() {
 	// dial
-	conn, err := net.Dial("tcp", ":8080")
+	conn, err := net.Dial("tcp", "127.0.0.1:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
 
+	// send http request: /mock
+	conn.Write([]byte("GET /mock HTTP/1.1\r\nHost: localhost:8080\r\n\r\n"))
+
+	// read 1Bps
+	buf := make([]byte, 1)
+
+	conn.SetReadBuffer(1)
+
+	size := 0
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+		size += n
+		fmt.Printf("read %d bytes\n", size)
+		time.Sleep(1 * time.Second)
+	}
 }
